1af6f1a38c6c3b03696f3f08f7e733e26d8bc975
enescankuranli <nefezann@gmail.com>
2021-08-20 14:37:58 +0300

    Frontend 2

---
+++ src/app/models/brand.ts
@@ -0,0 +1,5 @@
+export interface Brand{
+    BrandId:number,
+    BrandName:string;
+
+}
---
+++ src/app/models/car.ts
@@ -0,0 +1,9 @@
+export interface Car{
+    Id:number;
+    carName:string;
+    BrandId:number;
+    colourId:number;
+    modelYear:number;
+    dailyPrice:number;
+    description:string;
+}
